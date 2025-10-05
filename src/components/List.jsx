a19931931c094dae832de69d0ee22d6943606d08
Andrés Ortega <andythe20xd@gmail.com>
2025-10-05 16:44:17 -0300

    gestion de estado

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 
 function List({ data }) {
+  const [index, setIndex] = useState(1);
   //capturar el evento de click
-  function handleClick(e){
-    console.log(e)
+  function handleClick(i) {
+    setIndex(i);
   }
-  
+  /* OJO! para que funcione handleClick, no debe hacerse una llamada directa 'onClick={handleClick(idx)}' 
+  porque se ejecuta directamente, en vez de cuando se hace el evento de click. Usamos una funcion anonima para evitar esto.
+  */
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
-        <li onClick={handleClick} className="list-group-item" key={elemento}>
+      {data.map((elemento, idx) => (
+        <li
+          onClick={() => handleClick(idx)}
+          className={`list-group-item ${index == idx ? "active" : ""}`}
+          key={elemento}
+        >
           {elemento}
         </li>
       ))}
     </ul>
   );
 }
-
 
 export default List;
