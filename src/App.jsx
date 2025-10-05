0c977d631b4d0304795ed3709937905de43a399a
Andrés Ortega <andythe20xd@gmail.com>
2025-10-05 18:24:25 -0300

    renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  // codigo jsx, parecido a html con un par de excepciones
-  // despues se transforma -> React.createElement
-  const list = new Array("Tanjiro", "Goku", "Eren");
+  const list = [];
   const handleSelect = (elemento) => {
     console.log("Elemento seleccionado:", elemento);
   };
@@ -13,12 +11,26 @@ function App() {
     console.log("Imprimiendo:", elemento);
   };
 
+  /*
+  //Renderizado condicional
+  //contenido: almacenar la logica de qué renderizar
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin elementos para mostrar"
+  );
+  */
+
+  //ahora quiero que si no hay elementos no quiero renderizar nada.
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
+
   return (
     <>
       <Card>
         <CardBody title="Hola mundo" text="Este es el texto" />
-        <List data={list} onSelect={handleSelect} />
-        <List data={list} onSelect={handleSelect2} />
+        {contenido}
       </Card>
       <Button text="Click me" />
     </>
